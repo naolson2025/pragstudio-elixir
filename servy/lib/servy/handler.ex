59f8af41c5fbe9ad784cc2b68fc6be190defb898
Nick Olson <naolson.1819@gmail.com>
2023-11-29 08:04:41 -0600

    finished chapter 12 structs

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,4 +1,13 @@
 defmodule Servy.Handler do
+  @moduledoc "handles http requests"
+
+  @pages_path Path.expand("../pages", __DIR__)
+
+  import Servy.Plugins, only: [track: 1, rewrite_path: 1, log: 1]
+  import Servy.Parser, only: [parse: 1]
+  alias Servy.Conv
+
+  @doc "Transforms a request into a response"
   def handle(request) do
     request
     |> parse()
@@ -9,48 +18,9 @@ defmodule Servy.Handler do
     |> format_response()
   end
 
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts("Warning: #{path} is on the loose!")
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{ conv | path: "/wildthings" }
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(cov), do: IO.inspect(cov)
-
-  def parse(request) do
-    [method, path, _version] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
-
-    %{method: method, path: path, resp_body: "", status: nil}
-  end
-
   # def route(conv) do
   #   route(conv, conv.method, conv.path)
   # end
-
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
-    # this syntax below is shorthand for: Map.put(conv, :resp_body, "Bears, Lions, Tigers")
-    # this shorthand only works when the key already exists in the map
-    %{ conv | resp_body: "Bears, Lions, Tigers", status: 200}
-  end
-
-  def route(%{method: "GET", path: "/bears"} = conv) do
-    %{ conv | resp_body: "Teddy, Grizzly, Pooh", status: 200}
-  end
-
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
-    %{ conv | resp_body: "Bear #{id}", status: 200}
-  end
 
   # def route(%{method: "GET", path: "/about"} = conv) do
   #   file = Path.expand("../pages", __DIR__)
@@ -64,11 +34,29 @@ defmodule Servy.Handler do
   #   end
   # end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../pages", __DIR__)
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
+    # this syntax below is shorthand for: Map.put(conv, :resp_body, "Bears, Lions, Tigers")
+    # this shorthand only works when the key already exists in the map
+    %{ conv | resp_body: "Bears, Lions, Tigers", status: 200}
+  end
+
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
+    %{ conv | resp_body: "Teddy, Grizzly, Pooh", status: 200}
+  end
+
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
+    %{ conv | resp_body: "Bear #{id}", status: 200}
+  end
+
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+    @pages_path
     |> Path.join("about.html")
     |> File.read
     |> handle_file(conv)
+  end
+
+  def route(%Conv{ path: path } = conv) do
+    %{ conv | resp_body: "No #{path} found!", status: 404 }
   end
 
   def handle_file({:ok, content}, conv) do
@@ -83,29 +71,14 @@ defmodule Servy.Handler do
     %{ conv | resp_body: "File error: #{reason}", status: 200}
   end
 
-  def route(%{ path: path } = conv) do
-    %{ conv | resp_body: "No #{path} found!", status: 404 }
-  end
-
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
   end
 end
 
---
+++ servy/lib/servy/parser.ex
@@ -0,0 +1,14 @@
+defmodule Servy.Parser do
+
+  alias Servy.Conv
+
+  def parse(request) do
+    [method, path, _version] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %Conv{method: method, path: path}
+  end
+end
