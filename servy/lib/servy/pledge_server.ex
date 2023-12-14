4560b59d3dbb824f55f0ab8c16075e7ca60a2271
Nick Olson <naolson.1819@gmail.com>
2023-12-14 06:52:40 -0600

    finished chapter 25

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,75 +1,101 @@
-defmodule Servy.PledgeServer do
-  @name :pledge_server
-
+defmodule Servy.GenericServer do
   ## Client interface functions
   # called by the client to spawn the server process
-  def start do
-    IO.puts("Starting PledgeServer...")
-    pid = spawn(__MODULE__, :listen_loop, [[]])
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
     # register the pid with the name :pledge_server
     # we can choose any name we want, but we'll use :pledge_server
     # That way other functions can send messages to it
     # like a global variable
-    Process.register(pid, @name)
+    Process.register(pid, name)
     pid
   end
 
-  # This will run in the client process
-  # called by the client
-  # sends message to the listen_loop
-  def create_pledge(name, amount) do
-    send(@name, {self(), :create_pledge, name, amount})
+  # helper functions
+  def call(pid, message) do
+    send(pid, {:call, self(), message})
 
     receive do
-      {:response, status} -> status
+      {:response, response} -> response
     end
   end
 
-  def recent_pledges() do
-    send(@name, {self(), :recent_pledges})
-
-    receive do
-      {:response, pledges} -> pledges
-    end
-  end
-
-  def total_pledged do
-    send @name, {self(), :total_pledged}
-
-    receive do {:response, total} -> total end
+  def cast(pid, message) do
+    send(pid, {:cast, message})
   end
 
   ## Server functions
 
-  def listen_loop(state) do
+  def listen_loop(state, callback_module) do
     IO.puts("\nWaiting for a message...")
 
     receive do
-      {sender, :create_pledge, name, amount} ->
-        {:ok, id} = send_pledge_to_service(name, amount)
-        # take the first two pledges from the state
-        most_recent_pledges = Enum.take(state, 2)
-        # add the new pledge to the front of the list
-        # this way we always have the 3 most recent pledges
-        new_state = [{name, amount} | most_recent_pledges]
-        send(sender, {:response, id})
-        IO.puts("#{name} pledged #{amount}!")
-        IO.puts("New state is #{inspect(new_state)}")
-        listen_loop(new_state)
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send(sender, {:response, response})
+        listen_loop(new_state, callback_module)
 
-      {sender, :recent_pledges} ->
-        send(sender, {:response, state})
-        IO.puts("Sent recent pledges to #{inspect(sender)}")
-        listen_loop(state)
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
 
-      {sender, :total_pledged} ->
-        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
-        send sender, {:response, total}
-        listen_loop(state)
       unexpected ->
         IO.puts("Unexpected message: #{inspect(unexpected)}")
-        listen_loop(state)
+        listen_loop(state, callback_module)
     end
+  end
+end
+
+defmodule Servy.PledgeServer do
+  alias Servy.GenericServer
+  @name :pledge_server
+
+  # This will run in the client process
+  def start do
+    IO.puts("Starting PledgeServer...")
+    GenericServer.start(__MODULE__, [], @name)
+  end
+  # called by the client
+  # sends message to the listen_loop
+  def create_pledge(name, amount) do
+    GenericServer.call(@name, {:create_pledge, name, amount})
+  end
+
+  def recent_pledges() do
+    GenericServer.call(@name, :recent_pledges)
+  end
+
+  def total_pledged do
+    GenericServer.call(@name, :total_pledged)
+  end
+
+  def clear do
+    GenericServer.cast(@name, :clear)
+  end
+
+  def handle_cast(:clear, _state) do
+    []
+  end
+
+  def handle_call(:total_pledged, state) do
+    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
+    {total, state}
+  end
+
+  def handle_call(:recent_pledges, state) do
+    {state, state}
+  end
+
+  def handle_call({:create_pledge, name, amount}, state) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+    # take the first two pledges from the state
+    most_recent_pledges = Enum.take(state, 2)
+    # add the new pledge to the front of the list
+    # this way we always have the 3 most recent pledges
+    new_state = [{name, amount} | most_recent_pledges]
+    IO.puts("#{name} pledged #{amount}!")
+    IO.puts("New state is #{inspect(new_state)}")
+    {id, new_state}
   end
 
   defp send_pledge_to_service(_name, _amount) do
@@ -81,14 +107,17 @@ end
 
 alias Servy.PledgeServer
 
-pid = PledgeServer.start
+pid = PledgeServer.start()
 
-send pid, {:stop, "hammertime"}
+send(pid, {:stop, "hammertime"})
 
 IO.inspect(PledgeServer.create_pledge("larry", 10))
 IO.inspect(PledgeServer.create_pledge("moe", 20))
 IO.inspect(PledgeServer.create_pledge("curly", 30))
 IO.inspect(PledgeServer.create_pledge("daisy", 40))
+
+PledgeServer.clear()
+
 IO.inspect(PledgeServer.create_pledge("grace", 50))
 
 IO.inspect(PledgeServer.recent_pledges())
